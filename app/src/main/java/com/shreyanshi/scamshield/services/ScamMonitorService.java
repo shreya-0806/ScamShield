194b0bcfd74989b0a5fbb2e3a6e9e129e14dcf50
shreya-0806 <srivastavashreyanshi69@gmail.com>
2026-04-07 23:46:46 +0530

    updated agents and files

--- app/src/main/java/com/shreyanshi/scamshield/services/ScamMonitorService.java
+++ app/src/main/java/com/shreyanshi/scamshield/services/ScamMonitorService.java
@@ -9,6 +9,7 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ServiceInfo;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
